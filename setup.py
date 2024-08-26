96d9146a0bf5e0d775fee10842c1427469f1d0be
ravenvanewijk <ravenvanewijk1@gmail.com>
2024-08-26 15:25:51 +0200

    ts fixes + construct scen in package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name='roadroute-lib',
-    version='0.1.12',
+    version='0.1.13',
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
     author='Raven van Ewijk',
