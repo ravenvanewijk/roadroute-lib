bfb0d9f329c574ed7d1587c4e79aa1772f2b0870
ravenvanewijk <ravenvanewijk1@gmail.com>
2024-09-13 09:13:38 +0200

    tc abs error instead of error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name='roadroute-lib',
-    version='0.1.23',
+    version='0.1.24',
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
     author='Raven van Ewijk',
