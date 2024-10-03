52cb0af8fd7750f01dca5f5e925678d266a7416c
ravenvanewijk <ravenvanewijk1@gmail.com>
2024-10-03 09:49:42 +0200

    ver upgr

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name='roadroute-lib',
-    version='0.2.13',
+    version='0.3.0',
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
     author='Raven van Ewijk',
